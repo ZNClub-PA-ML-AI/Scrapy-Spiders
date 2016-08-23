d38a41b2f9c1d8de12e2e5367878a36f53a832db
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-08-23 11:10:26 +0530

    first tutorial

---
+++ Tutorials/tutorial/tutorial/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+"""
+Created on Tue Aug 23 09:50:27 2016
+
+@author: Nevil Dsouza
+"""
+
+import scrapy
+
+class DmozItem(scrapy.Item):
+    title = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.Field()
---
+++ Tutorials/tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 09:50:27 2016
+
+@author: Nevil Dsouza
+"""
+import scrapy
+
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
