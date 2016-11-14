ecf915da26a42dfbdfd726502ff8fa09e019271a
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-11-14 23:57:06 +0530

    livemint page 24 crawl successful

--- stocknews/stocknews/spiders/livemint_spider.py
+++ stocknews/stocknews/spiders/livemint_spider.py
@@ -9,6 +9,7 @@ Created on Mon Nov 14 23:16:56 2016
 """
 
 import scrapy
+from stocknews.items import StocknewsItem
 
 class LivemintSpider(scrapy.Spider):
     name = "livemint_spider"
@@ -16,16 +17,34 @@ class LivemintSpider(scrapy.Spider):
     allowed_domains = ["livemint.com"]
     
     start_urls = [
-        "http://www.livemint.com/Query/DIoW9PdSAJUlZsu7iBevDI/companies-opinion.html?facet=subSection&page=0"
+        "http://www.livemint.com/Query/DIoW9PdSAJUlZsu7iBevDI/companies-opinion.html?facet=subSection&page=24"
         
-    ]
-
+    ]    
+    
     def parse(self, response):
-        #path1 = "/html/body/div[@id='o-wrapper']/div[@class='wrapper']/section[@class='left-col']/div[@class='listing-box-container']/div[@class='listing-box'][1]/div[@class='text-box']/h2[@class='split-heading-strong']/a/text()"        
-        #path2 = "/html/body/div[@id='o-wrapper']/div[@class='wrapper']/section[@class='left-col']/div[@class='listing-box-container']/div[@class='listing-box'][1]/div[@class='text-box']/p[@class='intro']/a/text()"        
-        #path3 = "/html/body/div[@id='o-wrapper']/div[@class='wrapper']/section[@class='left-col']/div[@class='listing-box-container']/div[@class='listing-box'][1]/div[@class='text-box']/p[@class='date-box']/text()"        
-        path="/html/body/div[@id='o-wrapper']/div[@class='wrapper']/section[@class='left-col']/div[@class='listing-box-container']/div[@class='listing-box'][1]/div[@class='text-box']/h2[@class='split-heading-strong']/a/text()"
-        text = response.selector.xpath(path).extract()
-        with open("output.md", 'a') as f:
-            for i in text:
-                f.writelines("\n"+str(i))
+        
+        title_start = "/html/body/div[@id='o-wrapper']/div[@class='wrapper']/section[@class='left-col']/div[@class='listing-box-container']/div[@class='listing-box']["        
+        title_end = "]/div[@class='text-box']/h2[@class='split-heading-strong']/a/text()"        
+        intro_start = "/html/body/div[@id='o-wrapper']/div[@class='wrapper']/section[@class='left-col']/div[@class='listing-box-container']/div[@class='listing-box']["        
+        intro_end = "]/div[@class='text-box']/p[@class='intro']/a/text()"
+        date_start = "/html/body/div[@id='o-wrapper']/div[@class='wrapper']/section[@class='left-col']/div[@class='listing-box-container']/div[@class='listing-box']["        
+        date_end = "]/div[@class='text-box']/p[@class='date-box']/text()"        
+        
+        result=[]        
+        
+        for i in range(1,11):
+            
+            item = StocknewsItem()
+
+            path = title_start+str(i)+title_end
+            item['title'] = response.selector.xpath(path).extract()
+            
+            path = intro_start+str(i)+intro_end
+            item['intro'] = response.selector.xpath(path).extract()
+            
+            path = date_start+str(i)+date_end
+            item['date'] = response.selector.xpath(path).extract()
+            
+            result.append(item)
+        
+        return result
