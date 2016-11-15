372c54d948c8f17b291bc75a3445dee40f358e4f
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-11-15 08:59:35 +0530

    testing with all urls of livemint.com

--- stocknews/stocknews/spiders/livemint_spider.py
+++ stocknews/stocknews/spiders/livemint_spider.py
@@ -12,14 +12,55 @@ import scrapy
 from stocknews.items import StocknewsItem
 
 class LivemintSpider(scrapy.Spider):
-    name = "livemint_spider"
-
-    allowed_domains = ["livemint.com"]
     
-    start_urls = [
-        "http://www.livemint.com/Query/DIoW9PdSAJUlZsu7iBevDI/companies-opinion.html?facet=subSection&page=24"
+    #spider name
+    name = "livemint_spider"
+    #domains
+    allowed_domains = ["livemint.com"]
+    #urls
+    start_urls = []
+    for i in range(24,-1,-1):
         
-    ]    
+        url_company_opinion = "http://www.livemint.com/Query/DIoW9PdSAJUlZsu7iBevDI/companies-opinion.html?facet=subSection&page="
+
+        temp = url_company_opinion+str(i)
+        start_urls.append(temp)
+    
+        url_company_management = "http://www.livemint.com/Query/V1eAlpSAzt0kHm6oBnOvDI/management.html?facet=subSection&page=0"
+    
+        temp = url_company_management+str(i)
+        start_urls.append(temp)
+        
+        url_results = "http://www.livemint.com/Query/ZtZgviOVr74zwZ37eD9uDI/results.html?facet=subSection&page=0"
+    
+        temp = url_results+str(i)
+        start_urls.append(temp)
+        
+        url_people = "http://www.livemint.com/Query/lZy3FU0kP9Cso5deYypuDI/people.html?facet=subSection&page=0"
+
+        temp = url_people+str(i)
+        start_urls.append(temp)
+    
+        url_infotech = "http://www.livemint.com/Query/P8RBwcvO9gvJl6xh6wTNzO/infotech.html?facet=subSection&page=0"
+    
+        temp = url_infotech+str(i)
+        start_urls.append(temp)
+        
+        url_finservices = "http://www.livemint.com/Query/5jCbPmmTjmX6bvfyV5XlkJ/financial-services.html?facet=subSection&page=0"
+    
+        temp = url_finservices+str(i)
+        start_urls.append(temp)
+    
+        url_energy = "http://www.livemint.com/Query/hHUQJ3ncBXZBGH3eVyKlkJ/energy.html?facet=subSection&page=0"
+    
+        temp = url_energy+str(i)
+        start_urls.append(temp)
+
+        url_industry_opinion = "http://www.livemint.com/Query/t5YPD42JdoNxoDBxwNemkJ/industry-opinion.html?facet=subSection&page=0"
+    
+        temp = url_industry_opinion+str(i)
+        start_urls.append(temp)
+        
     
     def parse(self, response):
         
@@ -39,7 +80,14 @@ class LivemintSpider(scrapy.Spider):
             item = StocknewsItem()
 
             path = title_start+str(i)+title_end
-            item['title'] = response.selector.xpath(path).extract()
+            #item['title'] = response.selector.xpath(path).extract()
+            temp = response.selector.xpath(path).extract()
+            
+            #validity check            
+            if temp is None:
+                break
+            else:
+                item['title'] = temp
             
             path = intro_start+str(i)+intro_end
             item['intro'] = response.selector.xpath(path).extract()
