37b696ce58a47d8092a1ef57d11868dbb2a4c4ed
ZNevzz <dsouza.nevil45@gmail.com>
2017-02-17 11:26:27 +0530

    livemint remaining body posts scraped

--- stocknews/stocknews/spiders/livemint_spider.py
+++ stocknews/stocknews/spiders/livemint_spider.py
@@ -22,6 +22,59 @@ class LivemintSpider(scrapy.Spider):
     allowed_domains = ["livemint.com"]
     #urls
     start_urls = []
+        
+    file_name = 'livemint_data_2.csv'
+
+    df = pd.read_csv(file_name,encoding='iso-8859-1')
+    
+    start_urls = df['href'].tolist()
+
+        
+    def parse(self, response):
+        
+        path = "/html/body/div[@id='o-wrapper']/div[@class='wrapper']/section[@class='left-col']/div[@id='div_storyContent']/div[@class='story-content']/p"
+        body = response.selector.xpath(path).extract()
+        result=""
+        for i in body:
+            temp = re.sub("<p>", "", i)
+            temp = re.sub("</p>","",temp)
+            temp = re.sub("<i>The writer does not own shares in the above-mentioned companies.</i>","",temp)
+            result=result+temp
+        
+        result = result.strip()
+        return {'body':result}
+        
+
+'''
+2016-11-15 09:02:44 [scrapy] INFO: Dumping Scrapy stats:
+{'downloader/request_bytes': 130494,
+ 'downloader/request_count': 306,
+ 'downloader/request_method_count/GET': 306,
+ 'downloader/response_bytes': 2140065,
+ 'downloader/response_count': 306,
+ 'downloader/response_status_count/200': 201,
+ 'downloader/response_status_count/302': 105,
+ 'finish_reason': 'finished',
+ 'finish_time': datetime.datetime(2016, 11, 15, 3, 32, 44, 393673),
+ 'item_scraped_count': 2000,
+ 'log_count/DEBUG': 2306,
+ 'log_count/ERROR': 22,
+ 'log_count/INFO': 8,
+ 'response_received_count': 201,
+ 'scheduler/dequeued': 305,
+ 'scheduler/dequeued/memory': 305,
+ 'scheduler/enqueued': 305,
+ 'scheduler/enqueued/memory': 305,
+ 'start_time': datetime.datetime(2016, 11, 15, 3, 32, 33, 627249)}
+
+'''
+
+'''
+
+'''
+
+
+"""
     for i in range(24,-1,-1):
         
         if i in range(0,4):
@@ -60,12 +113,9 @@ class LivemintSpider(scrapy.Spider):
             url_industry_opinion = "http://www.livemint.com/Query/t5YPD42JdoNxoDBxwNemkJ/industry-opinion.html?facet=subSection&page=0"    
             temp = url_industry_opinion+str(i)
             start_urls.append(temp)
-#    file_name = 'livemint_href.csv'
-#
-#    df = pd.read_csv(file_name,encoding='iso-8859-1')
-#    
-#    start_urls = df['href'].tolist()
+"""    
 
+"""
     def parse(self, response):
         
         title_start = "/html/body/div[@id='o-wrapper']/div[@class='wrapper']/section[@class='left-col']/div[@class='listing-box-container']/div[@class='listing-box']["
@@ -107,45 +157,3 @@ class LivemintSpider(scrapy.Spider):
         
         return result
 """        
-    def parse(self, response):
-        
-        path = "/html/body/div[@id='o-wrapper']/div[@class='wrapper']/section[@class='left-col']/div[@id='div_storyContent']/div[@class='story-content']/p"
-        body = response.selector.xpath(path).extract()
-        result=""
-        for i in body:
-            temp = re.sub("<p>", "", i)
-            temp = re.sub("</p>","",temp)
-            temp = re.sub("<i>The writer does not own shares in the above-mentioned companies.</i>","",temp)
-            result=result+temp
-        
-        result = result.strip()
-        return {'body':result}
-        
-"""
-'''
-2016-11-15 09:02:44 [scrapy] INFO: Dumping Scrapy stats:
-{'downloader/request_bytes': 130494,
- 'downloader/request_count': 306,
- 'downloader/request_method_count/GET': 306,
- 'downloader/response_bytes': 2140065,
- 'downloader/response_count': 306,
- 'downloader/response_status_count/200': 201,
- 'downloader/response_status_count/302': 105,
- 'finish_reason': 'finished',
- 'finish_time': datetime.datetime(2016, 11, 15, 3, 32, 44, 393673),
- 'item_scraped_count': 2000,
- 'log_count/DEBUG': 2306,
- 'log_count/ERROR': 22,
- 'log_count/INFO': 8,
- 'response_received_count': 201,
- 'scheduler/dequeued': 305,
- 'scheduler/dequeued/memory': 305,
- 'scheduler/enqueued': 305,
- 'scheduler/enqueued/memory': 305,
- 'start_time': datetime.datetime(2016, 11, 15, 3, 32, 33, 627249)}
-
-'''
-
-'''
-
-'''
